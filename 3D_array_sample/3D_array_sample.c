f5a458ca05f4d25f56d645b9ad6f18678038c18e
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-05-15 21:23:07 +0800

    add 3D array program

---
+++ 3D_array_sample/3D_array_sample.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main ( void ) {
+
+    int array [ 3 ] [ 3 ] [ 3 ] ={   {
+                                        { 1, 2, 3 },
+                                        { 4, 5, 6 },
+                                        { 7, 8, 9 }
+                                    },
+                                    {
+                                        { 10, 11, 12 },
+                                        { 13, 14, 15 },
+                                        { 16, 17, 18 }
+                                    },
+                                    {
+                                        { 19, 20, 21 },
+                                        { 22, 23, 24 },
+                                        { 25, 26, 27 }
+                                    }
+                                };
+
+    int dim, row, col; //dim means dimension
+
+    for ( dim = 0; dim < 3; dim++ ) {
+        printf ( "\n\nTable %d", dim +1 );
+        for ( row = 0; row < 3; row++ ) {
+            printf ( "\n" );
+            for ( col = 0; col < 3; col++ ) {
+                
+                printf ( "%d ", array [ dim ] [ row ] [ col ] );
+            }
+        }
+        
+    }
+
+    return 0;
+}
