a58c7dc0e595ecb1932f30dea307ac454ca70074
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-06-12 23:24:01 +0800

    modify integer reversal program

--- reverse_an_integer/reverse-an-integer.c
+++ reverse_an_integer/reverse-an-integer.c
@@ -14,17 +14,37 @@ int main ( void ) {
   return 0;
 }
 
+/*
 void reverseInt ( int num ) {
 
   int rem, sum = 0;
 
   while ( num != 0 ) {
-
-    rem = num % 10;
-    sum = sum * 10 + rem;
-    num /= 10;
-
+    rem = num % 10; //extract last digit
+    sum = sum * 10 + rem; //add that last digit to sum
+    num /= 10; //remove last digit
   }
 
-  printf ( "%d", sum );
+  printf ( "Reversed integer: %d", sum );
+}
+*/
+
+void reverseInt ( int num ) {
+
+  int rem, sum = 0;
+  int isNegative = num < 0;
+
+  if ( isNegative ) //handles negative numbers
+    num = -num;
+
+  while ( num != 0 ) {
+    rem = num % 10; //extract last digit
+    sum = sum * 10 + rem; //add that last digit to sum
+    num /= 10; //remove last digit
+  }
+
+  if ( isNegative )
+    sum = -sum;
+
+  printf ( "Reversed integer: %d", sum );
 }
